803ea9f70af557caad19f7c0eaf5e8b48d79d175
nk8291 <nandkishor.bobade@duretechnologies.com>
2023-04-25 00:24:14 +0530

    Add new feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import Report from "./Report";
 
 function App() {
   return (
@@ -22,4 +24,53 @@ function App() {
   );
 }
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: null,
+      reportEmbedUrl: null
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.post(
+      "https://login.microsoftonline.com/common/oauth2/token",
+      {
+        grant_type: "client_credentials",
+        client_id: "004fe472-d42e-40f1-8c91-7e6e3f918535",
+        client_secret: "66s8Q~O0vs6AXzkKJF~PbPhx7IhWsV-EopEhkavx",
+        resource: "https://analysis.windows.net/powerbi/api"
+      }
+    );
+    const { access_token } = response.data;
+    this.setState({
+      token: access_token
+    });
+    const reportResponse = await axios.get("https://app.powerbi.com/view?r=eyJrIjoiODY1Y2JhMTQtODZlMi00NDM1LWEzOWItMmEzYzJjZGU5MWIwIiwidCI6ImViNDJjZGViLWI3YjUtNGE2ZC1iYzFjLWQxZWJjYmZjNDgyZSJ9", {
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
+    });
+    const { embedUrl } = reportResponse.data;
+    this.setState({
+      reportEmbedUrl: embedUrl
+    });
+  }
+
+  render() {
+    const { token, reportEmbedUrl } = this.state;
+    if (!token || !reportEmbedUrl) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div className="container">
+        <h1>React Power BI Embedding</h1>
+        <Report token={token} embedUrl={reportEmbedUrl} />
+      </div>
+    );
+  }
+}
+
 export default App;
+
---
+++ src/Report.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Report extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEmbedded: false
+    };
+  }
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "https://microsoft.github.io/PowerBI-JavaScript/demo/bower_components/powerbi-client/dist/powerbi.js";
+    script.async = true;
+    script.onload = () => {
+      this.embedReport();
+    };
+    document.body.appendChild(script);
+  }
+
+  embedReport() {
+    const { powerbi } = window;
+    const { token, embedUrl } = this.props;
+    const reportContainer = document.getElementById("reportContainer");
+    const reportConfig = {
+      type: "report",
+      accessToken: token,
+      embedUrl: embedUrl,
+      id: "7c849985-963e-403c-b7c7-e3f91fb56b8d",
+      permissions: 2,
+      settings: {
+        filterPaneEnabled: false,
+        navContentPaneEnabled: true
+      }
+    };
+    const report = powerbi.embed(reportContainer, reportConfig);
+    report
